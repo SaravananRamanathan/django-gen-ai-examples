213e2ee423d34793fe5dc3328c9020e75179a8d7
Saravanan Ramanathan <srvnnramanathan@gmail.com>
2025-07-15 10:20:09 +0530

    feat: init dynamic prompts. (#14)
    --re-structured prompts, now prompts are saved to db and edited using TinyMCE via Django Admin.
    --Dynamic context is added to prompts using `Django template engine`.

--- django_gen_ai_examples/apps/chat_bot/admin.py
+++ django_gen_ai_examples/apps/chat_bot/admin.py
@@ -1,3 +1,22 @@
-from django.contrib import admin
+"""
+Django Admin for chat_bot App.
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.db import models
+from tinymce.widgets import TinyMCE
+
+from .models import PromptTemplate
+
+
+@admin.register(PromptTemplate)
+class PromptTemplateAdmin(admin.ModelAdmin):
+    "PromptTemplate Model Django Admin"
+
+    list_display = ("name", "lookup_key")
+    search_fields = ("name", "lookup_key", "prompt_template")
+    prepopulated_fields = {"lookup_key": ("name",)}
+
+    formfield_overrides = {
+        models.TextField: {"widget": TinyMCE(attrs={"cols": 80, "rows": 60})},
+    }
--- django_gen_ai_examples/urls.py
+++ django_gen_ai_examples/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path("", RedirectView.as_view(pattern_name="chat_bot:chat_window", permanent=True)),
     path("", include("chat_bot.urls")),
     re_path(r'^api/', include(api_patterns)),
+    path("tinymce/", include("tinymce.urls")),
 ] + debug_toolbar_urls()
